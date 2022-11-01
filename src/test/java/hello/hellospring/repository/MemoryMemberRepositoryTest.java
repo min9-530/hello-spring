61533d7a9ea38bb5dc09f4501b5cd6b6cc2aacab
김밍슈 <kmsu2046@gmail.com>
2022-11-01 20:17:39 +0900

    테스트 케이스 작성

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -31,7 +31,7 @@ public class MemoryMemberRepositoryTest {
         member2.setName("spring2");
         repository.save(member2);
 
-        Member result = repository.findByName("spring2").get();
+        Member result = repository.findByName("spring1").get();
 
         assertThat(result).isEqualTo(member1);
 
